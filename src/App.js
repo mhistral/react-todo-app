560bd9a0848185c39f784269ef1ac1fd88b794ca
DBASYSTEM\Kimberly.Parrenas <Kimberly.Parrenas@dbaadvisory.com>
2023-02-06 16:33:42 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle> TODO LIST </PageTitle>
+    </div>
+  );
+}
+
+export default App;
